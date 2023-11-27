efc7603385a474d8a067b0651a039c587956a7e1
honda <masakazu.honda@siva-s.com>
2023-11-27 21:03:42 +0900

    Initial commit

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting server on port", webPort)
+
+	// define http Server
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
